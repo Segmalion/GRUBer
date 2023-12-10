8cbf6d7102c873c21c95086b1b13fa5ba0101aae
Volodymyr Melnychenko <greencapsul@gmail.com>
2023-12-10 09:42:24 +0200

    Марафет LITE версии

--- gruber-lite.bat
+++ gruber-lite.bat
@@ -1,4 +1,8 @@
+@echo off
+@chcp 65001>nul
+
 mkdir base\new
+
 C:\Windows\System32\wbem\wmic.exe bios get serialnumber > base\new\serial.txt
 C:\Windows\System32\wbem\wmic.exe computersystem get "Model","Manufacturer", "Name", "UserName" > base\new\info.txt
 C:\Windows\System32\wbem\wmic.exe bios get serialnumber >> base\new\info.txt
@@ -10,4 +14,7 @@ tool\NetworkInterfacesView_x32.exe /stext base\new\net1.txt
 tool\WifiHistoryView.exe /stext base\new\net2.txt
 start /wait tool\ESETLogCollector.exe /accepteula /Age:30 /NoTargets:Fw base\new\eset-log.zip
 tool\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArHG /f=base\new\audit.html /L=en
+
+rename base\new [num][date]viddil#serialnumber#kategoriya
+
 pause
