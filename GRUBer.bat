e1c1385ea821452f546a100e500c13f4a7d4647b
Volodymyr Melnychenko <greencapsul@gmail.com>
2023-12-18 17:22:51 +0200

    Обнова 181223

--- GRUBer.bat
+++ GRUBer.bat
@@ -19,46 +19,68 @@ set /p "respp=Vidpovidalniy: "
 for /f "usebackq tokens=2 delims==" %%i in (`C:\Windows\System32\wbem\wmic.exe bios get serialnumber /value`) do set serial=%%i
 @REM FOR /F "usebackq" %%a IN (`tool\GetInfo.exe`) DO set "serial=%%a"
 set age=30
-set "dir=[%num%]"
+set "dir=[%num%][date]viddil#serialnumber#kategoriya"
 set "dirF=[%num%][%ministamp%]%dep%#%serial%#%cat%"
+set dirF=%dirF: =% && set dir=%dir: =%
+set "dir=base\%dir%"
+set file
 
 @REM удаление пробелов в переменной dir
-set dirF=%dirF: =%
 
 @REM создание папки для данных
 echo. & echo ============================================ & echo.
-echo Create new dir "base\%dir%"...
+echo Create new dir "%dir%"...
 if not exist base mkdir base
-mkdir base\%dir% & echo COMPLETE! & echo.
+if not exist base\[%ministamp%] mkdir base\[%ministamp%]
+mkdir %dir% & echo COMPLETE! & echo.
 
 @REM сбор данных
 echo Save devices stat to fille...
-wmic computersystem get "Manufacturer"/format:list > base\%dir%\info.txt
-wmic computersystem get "Model" /format:list >> base\%dir%\info.txt
-wmic computersystem get "Name"/format:list >> base\%dir%\info.txt
-wmic bios get serialnumber /format:list >> base\%dir%\info.txt
-wmic nic where PhysicalAdapter=True get MACAddress,Name >> base\%dir%\info.txt
-wmic NICCONFIG where IPEnabled=True get Caption,IPAddress,MACAddress >> base\%dir%\info.txt
-wmic UserAccount where "Disabled=FALSE" get Caption,Name >> base\%dir%\info.txt
+set file=%dir%\info.txt
+C:\Windows\System32\wbem\wmic.exe computersystem get "Manufacturer"/format:list > %file%
+C:\Windows\System32\wbem\wmic.exe computersystem get "Model" /format:list >> %file%
+C:\Windows\System32\wbem\wmic.exe computersystem get "Name"/format:list >> %file%
+C:\Windows\System32\wbem\wmic.exe bios get serialnumber /format:list >> %file%
+C:\Windows\System32\wbem\wmic.exe nic where PhysicalAdapter=True get MACAddress,Name >> %file%
+C:\Windows\System32\wbem\wmic.exe NICCONFIG where IPEnabled=True get Caption,IPAddress,MACAddress >> %file%
+C:\Windows\System32\wbem\wmic.exe UserAccount where "Disabled=FALSE" get Caption,Name >> %file%
 
-echo %respp% > base\%dir%\descr.txt
-C:\Windows\System32\wbem\wmic.exe bios get serialnumber > base\%dir%\serial.txt & echo COMPLETE! & echo.
+set file=%dir%\descr.txt
+echo %respp% > %file%
+set file=%dir%\serial.txt
+C:\Windows\System32\wbem\wmic.exe bios get serialnumber > %file% & echo COMPLETE! & echo.
 
 echo Scan remote devices stat..
-tool\USBDeview_x32.exe /stext base\%dir%\usb.txt & echo COMPLETE! & echo.
+set file=%dir%\usb.txt
+tool\USBDeview_x32.exe /stext %dir%\usb.txt & echo COMPLETE! & echo.
 
 echo Scan net stat...
-tool\NetworkInterfacesView_x32.exe /stext base\%dir%\net1.txt
-tool\WifiHistoryView.exe /stext base\%dir%\net2.txt & echo COMPLETE! & echo.
+set file=%dir%\net1.txt
+tool\NetworkInterfacesView_x32.exe /stext %dir%\net1.txt
+set file=%dir%\net2.txt
+tool\WifiHistoryView.exe /stext %dir%\net2.txt & echo COMPLETE! & echo.
 
-echo ESET LogCollector run...
-start /wait cmd.exe /c "start /wait tool\ESETLogCollector.exe /accepteula /Lang:UKR /Age:%age% /NoTargets:Fw base\%dir%\eset-log.zip"
-echo COMPLETE! & echo.
+choice /c YN /t 3 /d Y /m "Run ESET LogCollector?"
+if %errorlevel%==1 (
+   set file=%dir%\eset-log.zip
+   echo ESET LogCollector run...
+   start /wait cmd.exe /c "start /wait tool\ESETLogCollector.exe /accepteula /Lang:UKR /Age:%age% /NoTargets:Fw %dir%\eset-log.zip"
+   echo COMPLETE!
+)
+echo.
 
-echo Scan pc sys stat...
-tool\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArHG /f=base\%dir%\audit.html /L=en
+choice /c YN /t 3 /d Y /m "Run PCsysStat?"
+if %errorlevel%==1 (
+   set file=%dir%\audit.html
+   echo Scan PCsysStat...
+   tool\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArHG /f=%dir%\audit.html /L=en
+   echo COMPLETE!
+)
+echo.
+
 echo ALL COMPLETE!!! & echo.
 
-rename base\%dir% %dirF%
+@rename %dir% %dirF%
+@move base\%dirF% base\[%ministamp%]\
 
 pause
--- gruber-lite.bat
+++ gruber-lite.bat
@@ -1,20 +1,21 @@
 @echo off
 @chcp 65001>nul
 
-mkdir base\new
+set "dir=[num][date]viddil#serialnumber#kategoriya"
+set "dir=base\%dir%"
 
-C:\Windows\System32\wbem\wmic.exe bios get serialnumber > base\new\serial.txt
-C:\Windows\System32\wbem\wmic.exe computersystem get "Model","Manufacturer", "Name", "UserName" > base\new\info.txt
-C:\Windows\System32\wbem\wmic.exe bios get serialnumber >> base\new\info.txt
-C:\Windows\System32\wbem\wmic.exe nic where PhysicalAdapter=True get MACAddress,Name >> base\new\info.txt
-C:\Windows\System32\wbem\wmic.exe NICCONFIG where IPEnabled=True get Caption,IPAddress,MACAddress >> base\new\info.txt
-C:\Windows\System32\wbem\wmic.exe UserAccount where "Disabled=FALSE" get Caption,Name >> base\new\info.txt
-tool\USBDeview_x32.exe /stext base\new\usb.txt
-tool\NetworkInterfacesView_x32.exe /stext base\new\net1.txt
-tool\WifiHistoryView.exe /stext base\new\net2.txt
-start /wait tool\ESETLogCollector.exe /accepteula /Age:30 /NoTargets:Fw base\new\eset-log.zip
-tool\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArHG /f=base\new\audit.html /L=en
+mkdir %dir%
 
-rename base\new [num][date]viddil#serialnumber#kategoriya
+C:\Windows\System32\wbem\wmic.exe bios get serialnumber > %dir%\serial.txt
+C:\Windows\System32\wbem\wmic.exe computersystem get "Model","Manufacturer", "Name", "UserName" > %dir%\info.txt
+C:\Windows\System32\wbem\wmic.exe bios get serialnumber >> %dir%\info.txt
+C:\Windows\System32\wbem\wmic.exe nic where PhysicalAdapter=True get MACAddress,Name >> %dir%\info.txt
+C:\Windows\System32\wbem\wmic.exe NICCONFIG where IPEnabled=True get Caption,IPAddress,MACAddress >> %dir%\info.txt
+C:\Windows\System32\wbem\wmic.exe UserAccount where "Disabled=FALSE" get Caption,Name >> %dir%\info.txt
+tool\USBDeview_x32.exe /stext %dir%\usb.txt
+tool\NetworkInterfacesView_x32.exe /stext %dir%\net1.txt
+tool\WifiHistoryView.exe /stext %dir%\net2.txt
+start /wait tool\ESETLogCollector.exe /accepteula /Age:30 /NoTargets:Fw %dir%\eset-log.zip
+tool\WinAudit.exe /r=gsoPxuTUeERNtnzDaIbMpmidcSArHG /f=%dir%\audit.html /L=en
 
 pause
